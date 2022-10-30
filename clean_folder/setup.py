63fc51c9d58e5436450625a4c9b13fd29e226940
SerhioKAS <116594964+SerhioKAS@users.noreply.github.com>
2022-10-30 20:55:52 +0200

    Create setup.py

---
+++ clean_folder/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='1.0.0',
+    description='Script for sorting entire files in selected folder',
+    url='http://github.com/SerhioKAS/Go_IT9_Kashpurenko_HW_7',
+    author='Kashpurenko',
+    author_email='sarhiokas@gmail.com',
+    license='MIT',
+    platforms='windows, unix',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts' : ['clean-folder = clean_folder.clean: start_func']}
+)
